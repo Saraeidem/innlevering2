e72c3dbbbf52b96ac1dc76421081196407458fb8
Sara Bolsø Eidem <saeid8969@usn.no>
2025-10-28 09:39:39 +0100

    Oppdatert db.php med riktig databasepassord

--- db.php
+++ db.php
@@ -1,19 +1,11 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-/* --- KOBLING TIL SKOLENS DATABASE (DOKPLOY) --- */
-$host = "b-studentsql-1.usn.no";   // fra SelfService
-$user = "saeid8969";               // fra SelfService
-$pass = "DittPassordHer";          // fra SelfService
-$dbname = "saeid8969";             // fra SelfService
-$port = 3306;                      // som regel 3306
+$host   = 'b-studentsql-1.usn.no'; // fra SelfService
+$user   = 'saeid8969';             // fra SelfService
+$pass   = 'ad3esaeid8969';     // fra SelfService
+$dbname = 'saeid8969';             // fra SelfService
+$port   = 3306;
 
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
-$conn->set_charset("utf8mb4");
-
-/* valgfri hjelpefunksjon */
-function input($key, $src='POST') {
-  $a = $src === 'GET' ? $_GET : $_POST;
-  return isset($a[$key]) ? trim($a[$key]) : null;
-}
-?>
+$conn->set_charset('utf8mb4');
