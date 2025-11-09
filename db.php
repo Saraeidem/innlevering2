914817cf64973d5a4a470fe9988ae8817aac4f58
Sara Bolsø Eidem <saeid8969@usn.no>
2025-11-09 17:04:38 +0100

    Din commit-melding her

--- db.php
+++ db.php
@@ -1,18 +1,17 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-/* ==== DB-tilkobling ==== */
 $host   = 'b-studentsql-1.usn.no';
 $user   = 'saeid8969';
-$pass   = 'ad3seaeid8969';   // ditt ekte passord
+$pass   = 'ad3esaeid8969';   
 $dbname = 'saeid8969';
 $port   = 3306;
 
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 $conn->set_charset('utf8mb4');
 
-/* ==== Hjelpefunksjon brukt i alle sidene ==== */
 function input($key, $src = 'POST') {
     $a = ($src === 'GET') ? $_GET : $_POST;
     return isset($a[$key]) ? trim($a[$key]) : null;
 }
+
